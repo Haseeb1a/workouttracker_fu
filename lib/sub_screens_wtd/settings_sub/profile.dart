af73a420e144dbc9d4fbef01e8067ff07417782b
Haseeb1a <mh3824555@gmail.com>
2023-08-17 13:57:47 +0530

    testing2

--- lib/sub_screens_wtd/settings_sub/profile.dart
+++ lib/sub_screens_wtd/settings_sub/profile.dart
@@ -1,107 +1,107 @@
-// import 'dart:io';
+import 'dart:io';
 
-// import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
-// import 'package:workouttraker/function/db_functions/db_functions.dart';
-// import 'package:workouttraker/function/db_functions/db_person.dart';
-// import 'package:workouttraker/model/person_model/persondata.dart';
-// import 'package:workouttraker/model/task_model/workoutmodel1.dart';
-// import 'package:image_picker/image_picker.dart';
-// import 'package:workouttraker/sub_screens_wtd/other_sub/profileedit.dart';
-// // import 'package:workouttraker/dbfunction/model/workoutmodel.dart';
-// // import 'package:workouttraker/dbfunction/model/workoutmodel1.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:workouttraker/function/db_functions/db_functions.dart';
+import 'package:workouttraker/function/db_functions/db_person.dart';
+import 'package:workouttraker/model/person_model/persondata.dart';
+import 'package:workouttraker/model/task_model/workoutmodel1.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:workouttraker/sub_screens_wtd/other_sub/profileedit.dart';
+// import 'package:workouttraker/dbfunction/model/workoutmodel.dart';
+// import 'package:workouttraker/dbfunction/model/workoutmodel1.dart';
 
-// // class Profile extends StatelessWidget {
-// //   const Profile({Key? key});
+// class Profile extends StatelessWidget {
+//   const Profile({Key? key});
 
-// //   @override
-// //   Widget build(BuildContext context) {
-// //       double screenWidth = MediaQuery.of(context).size.width;
-// //     double screenHeight = MediaQuery.of(context).size.height;
-// //     // Your existing build method implementation
+//   @override
+//   Widget build(BuildContext context) {
+//       double screenWidth = MediaQuery.of(context).size.width;
+//     double screenHeight = MediaQuery.of(context).size.height;
+//     // Your existing build method implementation
 
-// //     // Replace the hardcoded data with the ValueListenableBuilder to read data from Hive
-// //     return ValueListenableBuilder<List<persondata>>(
-// //       valueListenable:persondataListNotifier  ,
-// //       builder: (context, persondatalist, _) {
-// //         final data = persondatalist[index];
-// //         // Assuming you only have one item in the list for simplicity
-// //         // final workoutModel = workoutList.isNotEmpty ? workoutList.first : null;
+//     // Replace the hardcoded data with the ValueListenableBuilder to read data from Hive
+//     return ValueListenableBuilder<List<persondata>>(
+//       valueListenable:persondataListNotifier  ,
+//       builder: (context, persondatalist, _) {
+//         final data = persondatalist[index];
+//         // Assuming you only have one item in the list for simplicity
+//         // final workoutModel = workoutList.isNotEmpty ? workoutList.first : null;
 
-// //         return Scaffold(
-// //           backgroundColor: Color.fromARGB(255, 0, 0, 0),
-// //           appBar: AppBar(
-// //             backgroundColor: Color.fromARGB(225, 27, 57, 61),
-// //             title: Center(
-// //               child: Text('profile'),
-// //             ),
-// //           ),
-// //           body: Center(
-// //             child: Card(
-// //               color: Color.fromARGB(225, 27, 57, 61),
-// //               child: Row(
-// //                 mainAxisAlignment: MainAxisAlignment.center,
-// //                 crossAxisAlignment: CrossAxisAlignment.center,
-// //                 children: [
-// //                   Stack(
-// //                     children: [
-// //                       Container(
-// //                         width: screenHeight * 0.2,
-// //                         height: screenHeight * 0.9,
-// //                         alignment: Alignment.center,
-// //                         decoration: BoxDecoration(
-// //                           borderRadius: BorderRadius.circular(15),
-// //                         ),
-// //                         child: ClipRRect(
-// //                           borderRadius: BorderRadius.circular(30.0),
-// //                           child: Image.asset('assets/images/full body.png'),
-// //                         ),
-// //                       ),
-// //                     ],
-// //                   ),
-// //                   SizedBox(
-// //                     width: screenWidth * 0.02,
-// //                   ),
-// //                   SizedBox(
-// //                     width: screenWidth * 0.47,
-// //                     height: screenHeight * 0.52,
-// //                     child: Card(
-// //                       elevation: 10,
-// //                       color: Color.fromARGB(255, 103, 109, 75),
-// //                       child: Center(
-// //                         // child: workoutModel != null
-// //                             // ? 
-// //                            child: Text(
-// //                                 '''
-// // Name:adil
-// // age: 22
-// // weight: 55
-// // height: 158
-// //                                 ''',
-// //                                 //  Name: ${workoutModel.personname}
-// //                                 // age: ${workoutModel.personage}
-// //                                 // weight: ${workoutModel.personweight}
-// //                                 // height: ${workoutModel.personheight}
-// //                                 style: GoogleFonts.alegreyaSc(
-// //                                   fontSize: 27,
-// //                                   color: Colors.white,
-// //                                 ),
-// //                                 textAlign: TextAlign.center,
-// //                               ),
-// //                             // : CircularProgressIndicator(), // Show loading indicator if data is not available yet
-// //                       ),
-// //                     ),
-// //                   )
-// //                 ],
-// //               ),
-// //             ),
-// //           ),
-// //         );
-// //       },
-// //     );
-// //   }
-// // }
-// // --------------------------------------------------------------------
+//         return Scaffold(
+//           backgroundColor: Color.fromARGB(255, 0, 0, 0),
+//           appBar: AppBar(
+//             backgroundColor: Color.fromARGB(225, 27, 57, 61),
+//             title: Center(
+//               child: Text('profile'),
+//             ),
+//           ),
+//           body: Center(
+//             child: Card(
+//               color: Color.fromARGB(225, 27, 57, 61),
+//               child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 crossAxisAlignment: CrossAxisAlignment.center,
+//                 children: [
+//                   Stack(
+//                     children: [
+//                       Container(
+//                         width: screenHeight * 0.2,
+//                         height: screenHeight * 0.9,
+//                         alignment: Alignment.center,
+//                         decoration: BoxDecoration(
+//                           borderRadius: BorderRadius.circular(15),
+//                         ),
+//                         child: ClipRRect(
+//                           borderRadius: BorderRadius.circular(30.0),
+//                           child: Image.asset('assets/images/full body.png'),
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                   SizedBox(
+//                     width: screenWidth * 0.02,
+//                   ),
+//                   SizedBox(
+//                     width: screenWidth * 0.47,
+//                     height: screenHeight * 0.52,
+//                     child: Card(
+//                       elevation: 10,
+//                       color: Color.fromARGB(255, 103, 109, 75),
+//                       child: Center(
+//                         // child: workoutModel != null
+//                             // ? 
+//                            child: Text(
+//                                 '''
+// Name:adil
+// age: 22
+// weight: 55
+// height: 158
+//                                 ''',
+//                                 //  Name: ${workoutModel.personname}
+//                                 // age: ${workoutModel.personage}
+//                                 // weight: ${workoutModel.personweight}
+//                                 // height: ${workoutModel.personheight}
+//                                 style: GoogleFonts.alegreyaSc(
+//                                   fontSize: 27,
+//                                   color: Colors.white,
+//                                 ),
+//                                 textAlign: TextAlign.center,
+//                               ),
+//                             // : CircularProgressIndicator(), // Show loading indicator if data is not available yet
+//                       ),
+//                     ),
+//                   )
+//                 ],
+//               ),
+//             ),
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
+// --------------------------------------------------------------------
 // class Profile extends StatefulWidget {
 //   const Profile({Key? key});
 
@@ -231,6 +231,7 @@
 // Age: ${data.personage}
 // Weight: ${data.personweight}
 // Height: ${data.personheight}
+// height:${data.personImage}
 //                                 ''',
 //                                 style: GoogleFonts.alegreyaSc(
 //                                   fontSize: 27,
@@ -254,8 +255,10 @@
 //   }
 // }
 //--------------------------------------------------------3
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:workouttraker/function/db_functions/db_person.dart';
@@ -272,14 +275,35 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   File? _selectedImage; // Variable to store the selected image
 
+  late persondata data; // Declare data variable here
+
+  @override
+  void initState() {
+    super.initState();
+    data = persondataListNotifier.value.first; // Initialize data with the first item
+  }
+
   // Function to open the image picker
   Future<void> _pickImage() async {
     final pickedImage = await ImagePicker().pickImage(source: ImageSource.gallery);
 
     if (pickedImage != null) {
+      final imageBytes = await pickedImage.readAsBytes();
+
       setState(() {
         _selectedImage = File(pickedImage.path);
       });
+
+      final updatedData = persondata(
+        personname: data.personname,
+        personage: data.personage,
+        personweight: data.personweight,
+        personheight: data.personheight,
+        id: data.id,
+        personImage: base64Encode(imageBytes), // Store the image as a base64-encoded string
+      );
+
+      updateperson(0, updatedData); // Assuming you're updating the first item
     }
   }
 
@@ -294,8 +318,6 @@ class _ProfileState extends State<Profile> {
         if (persondatalist.isEmpty) {
           return CircularProgressIndicator(); // Show loading indicator if data is not available yet
         }
-
-        final data = persondatalist[0]; // Assuming you want to display the first item
 
         return Scaffold(
           backgroundColor: Color.fromARGB(255, 0, 0, 0),
@@ -400,6 +422,7 @@ Name: ${data.personname}
 Age: ${data.personage}
 Weight: ${data.personweight}
 Height: ${data.personheight}
+Image: ${data.personImage ?? 'No Image'} 
                                 ''',
                                 style: GoogleFonts.alegreyaSc(
                                   fontSize: 27,
